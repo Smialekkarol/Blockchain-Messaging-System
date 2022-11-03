b9b32a08b7ce112805072b8e1325a7c4e3a08a8b
qqwertyui <p.warmke@wp.pl>
2022-11-03 05:05:10 +0100

    Add control channel message processing pt.1

--- node/src/Consumer.cpp
+++ node/src/Consumer.cpp
@@ -1,36 +1,67 @@
 #include "Consumer.hpp"
 #include "Utils.hpp"
-#include "common/ConnectionHandler.hpp"
+#include "common/utils/Text.hpp"
 #include <amqpcpp.h>
 #include <spdlog/spdlog.h>
 #include <thread>
 
 void Consumer::run(const common::NodeConfiguration &config) {
-  std::thread{Consumer::main, config}.detach();
+  std::thread{[&config]() { Consumer{config}.run(); }}.detach();
 }
 
-void Consumer::main(const common::NodeConfiguration &config) {
-  auto *loop = ev_loop_new(0);
-  common::ConnectionHandler handler(loop);
-  AMQP::TcpConnection connection(&handler, AMQP::Address(config.self.address));
-  std::string channelToBeCreated;
-  AMQP::TcpChannel channel(&connection);
+Consumer::Consumer(const common::NodeConfiguration &config)
+    : loop(ev_loop_new(0)), handler(loop),
+      connection(&handler, AMQP::Address(config.self.address)),
+      channel(&connection) {
   channel.onError(
       [](const char *message) { spdlog::error("Channel error: {}", message); });
-
   channel.declareQueue(config.self.name + "_ControlChannel", AMQP::durable)
       .onError([](const char *message) { spdlog::error(message); });
-
   channel.consume(config.self.name + "_ControlChannel", AMQP::noack)
       .onReceived([&](const AMQP::Message &msg, uint64_t tag,
                       bool redelivered) {
         std::string message{msg.body(), msg.body() + msg.bodySize()};
         spdlog::debug(config.self.name + "_ControlChannel" + " Received: {}",
                       message);
-        channelToBeCreated = message;
-        channel.declareQueue(channelToBeCreated, AMQP::durable)
-            .onError([](const char *message) { spdlog::error(message); });
+        auto tokens = common::utils::Text::tokenizeBySpace(message);
+        handleControlMessage(tokens);
       });
+}
 
-  ev_run(loop, 0);
+Consumer::~Consumer() { ev_loop_destroy(loop); }
+
+void Consumer::run() { ev_run(loop, 0); }
+
+void Consumer::handleControlMessage(const gsl::span<std::string> &command) {
+  const auto &numberOfArgs = command.size();
+  if (numberOfArgs == 0) {
+    spdlog::error("Empty message");
+    return;
+  }
+  const auto &operation = command[0];
+  const auto &args = command.subspan(1, command.size() - 1);
+  if (operation == "create") {
+    createChannel(args);
+  } else if (operation == "history") {
+    //
+  } else {
+    spdlog::error("Invalid command");
+  }
+}
+
+void Consumer::createChannel(const gsl::span<std::string> &command) {
+  if (command.size() == 0) {
+    spdlog::error("Invalid number of args");
+    return;
+  }
+  std::string channelToBeCreated = command[0];
+  channel.declareQueue(channelToBeCreated, AMQP::durable)
+      .onError([](const char *message) { spdlog::error(message); });
+  channel.consume(channelToBeCreated, AMQP::noack)
+      .onReceived([&channelToBeCreated](const AMQP::Message &msg, uint64_t tag,
+                                        bool redelivered) {
+        std::string message{msg.body(), msg.body() + msg.bodySize()};
+        spdlog::debug("Received on channel \"{}\": {}", channelToBeCreated,
+                      message);
+      });
 }
--- node/src/Consumer.hpp
+++ node/src/Consumer.hpp
@@ -1,11 +1,29 @@
 #pragma once
 
+#include "common/ConnectionHandler.hpp"
 #include "common/NodeConfiguration.hpp"
+#include <amqpcpp.h>
+#include <ev.h>
+#include <gsl/gsl>
 
 class Consumer {
 public:
+  Consumer(const common::NodeConfiguration &config);
+  Consumer(const Consumer &) = delete;
+  Consumer(Consumer &&) = delete;
+  Consumer &operator=(const Consumer &) = delete;
+  Consumer &operator=(Consumer &&) = delete;
+  ~Consumer();
+
   static void run(const common::NodeConfiguration &config);
 
 private:
-  static void main(const common::NodeConfiguration &config);
+  struct ev_loop *loop;
+  common::ConnectionHandler handler;
+  AMQP::TcpConnection connection;
+  AMQP::TcpChannel channel;
+
+  void run();
+  void handleControlMessage(const gsl::span<std::string> &command);
+  void createChannel(const gsl::span<std::string> &command);
 };
