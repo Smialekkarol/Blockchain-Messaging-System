7e90142af62b711f00b25385a58df60180951bb9
DCTRN <michal.pytlik98@gmail.com>
2022-12-27 01:13:43 +0000

    Add option to set number of clients to simulate chat.

--- client/src/App.cpp
+++ client/src/App.cpp
@@ -1,84 +1,105 @@
+#include "Client.hpp"
 #include "Utils.hpp"
 #include <iostream>
-#include "Client.hpp"
+#include <memory>
+#include <string>
+#include <vector>
 
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/spdlog.h>
 
-int main(int argc, char** argv)
-{
-    if (argc != 3)
-    {
-        std::cerr << "Usage: Client <ServerName> <ClientName>\n"
-                  << "Example:\n"
-                  << "    ./Client NodeA ClientName";
-        return EXIT_FAILURE;
+#include "common/itf/Constants.hpp"
+#include "common/utils/Time.hpp"
+#include "common/utils/Timer.hpp"
+
+int main(int argc, char **argv) {
+  if (argc != 3) {
+    std::cerr << "Usage: Client <ServerName> <ClientName>\n"
+              << "Example:\n"
+              << "    ./Client NodeA ClientName";
+    return EXIT_FAILURE;
+  }
+
+  const auto &serverName = argv[1];
+  const auto &clientName = argv[2];
+
+  spdlog::set_level(spdlog::level::debug);
+  auto defaultFileLogger =
+      spdlog::basic_logger_mt("default_logger", "logs/client/logs.txt", true);
+  spdlog::set_default_logger(defaultFileLogger);
+
+  const std::string &nodeAddress = readNodes("Nodes.yaml", serverName);
+
+  client::ClientInfo clientInfo(nodeAddress, serverName, clientName);
+  client::Client client(clientInfo);
+
+  const std::uint32_t numberOfClients{3};
+  std::vector<std::shared_ptr<client::Client>> clients{};
+
+  for (std::uint32_t i{0}; i < numberOfClients; i++) {
+    client::ClientInfo clientInfoLocal(nodeAddress, serverName,
+                                       std::string{clientName} + "_" +
+                                           std::to_string(i));
+    clients.push_back(std::make_shared<client::Client>(clientInfoLocal));
+  }
+
+  std::string command{};
+
+  std::uint64_t counter{0};
+  client.createQueue(::common::itf::DEFAULT_BLOCKAIN);
+  ::common::utils::Timer timer{};
+  timer.setInterval(
+      [&clients, &counter]() {
+        for (auto &c : clients) {
+          c->sendMessage(::common::itf::DEFAULT_BLOCKAIN,
+                         "message" + std::to_string(counter));
+        }
+        counter++;
+      },
+      ::common::utils::Time::SLOT);
+
+  bool shouldExit{false};
+  while (not shouldExit) {
+    std::cout << ">>> ";
+    std::getline(std::cin, command);
+    if (command.empty()) {
+      continue;
+    }
+    auto args = tokenizeBySpace(command);
+    const auto operation = toLower(args[0]);
+
+    if (operation.compare("init") == 0) {
+      if (args.size() == 2) {
+        const auto &queToBeCreated = args[1];
+        client.createQueue(queToBeCreated);
+      } else
+        std::cout << "initChannel: <queToBeCreated>\n";
     }
 
-    const auto& serverName = argv[1];
-    const auto& clientName = argv[2];
-
-    const std::string & nodeAddress = readNodes("Nodes.yaml", serverName);
-
-    client::ClientInfo clientInfo(nodeAddress, serverName, clientName);
-    client::Client client(clientInfo);
-
-    std::string command{};
-
-    bool shouldExit{ false };
-    while (not shouldExit)
-    {
-        std::cout << ">>> ";
-        std::getline(std::cin, command);
-        if (command.empty())
-        {
-            continue;
-        }
-        auto args = tokenizeBySpace(command);
-        const auto operation = toLower(args[0]);
-
-        if (operation.compare("init") == 0)
-        {
-            if (args.size() == 2)
-            {
-                const auto& queToBeCreated = args[1];
-                client.createQueue(queToBeCreated);
-            }
-            else
-                std::cout << "initChannel: <queToBeCreated>\n";
-        }
-
-        if (operation.compare("send") == 0)
-        {
-            if (args.size() == 3)
-            {
-                const std::string & queName = args[1];
-                const std::string & message = args[2];
-                client.sendMessage(queName, message);
-            }
-            else
-                std::cout << "send: <queName> <message>\n";
-        }
-
-        if (operation.compare("getMessages") == 0)
-        {
-            if (args.size() == 3)
-            {
-                const auto& queName = args[1];
-                const auto& date = args[2];
-                client.getMessages(queName, date);
-            }
-            else
-                std::cout << "getMessages: <queName> <date>\n";
-        }
-
-        else if (operation.compare("exit") == 0)
-        {
-            shouldExit = true;
-        }
-        else if (operation.compare("help") == 0)
-        {
-            std::cout << "Possible commands: [init, send, exit]\n";
-        }
+    if (operation.compare("send") == 0) {
+      if (args.size() == 3) {
+        const std::string &queName = args[1];
+        const std::string &message = args[2];
+        client.sendMessage(queName, message);
+      } else
+        std::cout << "send: <queName> <message>\n";
     }
 
-    return 0;
+    if (operation.compare("getMessages") == 0) {
+      if (args.size() == 3) {
+        const auto &queName = args[1];
+        const auto &date = args[2];
+        client.getMessages(queName, date);
+      } else
+        std::cout << "getMessages: <queName> <date>\n";
+    }
+
+    else if (operation.compare("exit") == 0) {
+      shouldExit = true;
+    } else if (operation.compare("help") == 0) {
+      std::cout << "Possible commands: [init, send, exit]\n";
+    }
+  }
+
+  return 0;
 }
--- client/src/Client.cpp
+++ client/src/Client.cpp
@@ -1,109 +1,96 @@
-#include "Client.hpp"
+#include <spdlog/spdlog.h>
+
+#include "common/utils/Text.hpp"
 #include "common/utils/Time.hpp"
 #include "common/utils/Timestamp.hpp"
-#include "common/utils/Text.hpp"
 
-namespace client
-{
+#include "Client.hpp"
 
-Client::Client(ClientInfo& clientInfo)
-    : clientInfo(clientInfo)
-    , amqpHandler(clientInfo.host)
-{
-    std::string queName{ clientInfo.clientName + "_ClientChannel" };
-    amqpHandler.createQueue(queName);
-    listen(queName);
+namespace client {
+
+Client::Client(ClientInfo &clientInfo)
+    : clientInfo(clientInfo), amqpHandler(clientInfo.host) {
+  std::string queName{clientInfo.clientName + "_ClientChannel"};
+  amqpHandler.createQueue(queName);
+  listen(queName);
 }
 
-Client::~Client()
-{
+Client::~Client() {}
+
+void Client::createQueue(const std::string &queToBeCreated) {
+  try {
+    std::string queName = clientInfo.serverName + "_ControlChannel";
+
+    common::itf::Header header("CreateChannel", clientInfo.serverName, queName,
+                               clientInfo.clientName);
+    common::itf::Message message(common::utils::Timestamp::get(),
+                                 queToBeCreated, clientInfo.clientName);
+
+    const std::string &data = serializeData(header, message);
+    amqpHandler.sendMessage(queName, data);
+  } catch (const std::exception &e) {
+    spdlog::error(e.what());
+  }
 }
 
-void Client::createQueue(const std::string& queToBeCreated)
-{
-    try
-    {
-        std::string queName = clientInfo.serverName + "_ControlChannel";
+void Client::sendMessage(const std::string &queName,
+                         const std::string &dataToSend) {
+  try {
+    common::itf::Header header("Send", clientInfo.serverName, queName,
+                               clientInfo.clientName);
+    common::itf::Message message(common::utils::Timestamp::get(), dataToSend,
+                                 clientInfo.clientName);
 
-        common::itf::Header header(
-            "CreateChannel", clientInfo.serverName, queName, clientInfo.clientName);
-        common::itf::Message message(
-            common::utils::Timestamp::get(), queToBeCreated, clientInfo.clientName);
+    const std::string &data = serializeData(header, message);
+    amqpHandler.sendMessage(queName, data);
+  } catch (const std::exception &e) {
+    spdlog::error(e.what());
+  }
+}
 
-        const std::string & data = serializeData(header, message);
-        amqpHandler.sendMessage(queName, data);
+std::vector<common::itf::Message>
+Client::getMessages(const std::string &queName,
+                    const std::string &RequestData) {
+  try {
+    common::itf::Header header("getMessages", clientInfo.serverName, queName,
+                               clientInfo.clientName);
+    common::itf::Message message(common::utils::Timestamp::get(), RequestData,
+                                 clientInfo.clientName);
+
+    const std::string &data = serializeData(header, message);
+  } catch (const std::exception &e) {
+    spdlog::error(e.what());
+  }
+  return {};
+}
+
+void Client::listen(const std::string &queue) {
+  std::thread{[this, queue]() {
+    amqpHandler.receive(queue, buffer).runEventLoop();
+  }}.detach();
+
+  auto handleBufferedData = [this]() {
+    if (not buffer.isEmpty()) {
+      std::scoped_lock(mutex);
+      for (const auto &msg : buffer.getData()) {
+        std::vector<std::string> data =
+            common::utils::Text::splitBySeparator(msg, "@");
+        const std::string &serializedMessage = data.at(0);
+        auto message = messageSerializer.deserialize(serializedMessage);
+        spdlog::info("[{}] {}: {}", common::utils::Time::getCurrentTime(),
+                     message.author, message.data);
+        std::cout << common::utils::Time::getCurrentTime() << " "
+                  << message.author << ": " << message.data << std::endl;
+      }
+      buffer.clearBuffer();
     }
-    catch (const std::exception& e)
-    {
-        spdlog::error(e.what());
-    }
+  };
+  timer.setInterval(handleBufferedData, 200);
 }
 
-void Client::sendMessage(const std::string& queName, const std::string& dataToSend)
-{
-    try
-    {
-        common::itf::Header header("Send", clientInfo.serverName, queName, clientInfo.clientName);
-        common::itf::Message message(
-            common::utils::Timestamp::get(), dataToSend, clientInfo.clientName);
-
-        const std::string& data = serializeData(header, message);
-        amqpHandler.sendMessage(queName, data);
-    }
-    catch (const std::exception& e)
-    {
-        spdlog::error(e.what());
-    }
+std::string Client::serializeData(common::itf::Header &header,
+                                  common::itf::Message &message) {
+  return headerSerializer.serialize(header) + "@" +
+         messageSerializer.serialize(message) + "\n";
 }
-
-std::vector<common::itf::Message> Client::getMessages(const std::string& queName, const std::string& RequestData)
-{
-    try
-    {
-        common::itf::Header header(
-            "getMessages", clientInfo.serverName, queName, clientInfo.clientName);
-        common::itf::Message message(
-            common::utils::Timestamp::get(), RequestData, clientInfo.clientName);
-
-        const std::string& data = serializeData(header, message);
-    }
-    catch (const std::exception& e)
-    {
-        spdlog::error(e.what());
-    }
-    return {};
-}
-
-void Client::listen(const std::string& queue)
-{
-    std::thread{
-        [this, queue]()
-        {
-           amqpHandler.receive(queue, buffer).runEventLoop();
-        }
-    }.detach();
-
-    auto handleBufferedData = [this]()
-    {
-        if (not buffer.isEmpty())
-        {
-            std::scoped_lock(mutex);
-            for (const auto& msg : buffer.getData())
-            {
-                std::vector<std::string> data = common::utils::Text::splitBySeparator(msg, "@");
-                const std::string& serializedMessage = data.at(0);
-                auto message = messageSerializer.deserialize(serializedMessage);
-                std::cout << common::utils::Time::getCurrentTime() << " " << message.author << ": "
-                          << message.data << std::endl;
-            }
-            buffer.clearBuffer();
-        }
-    };
-    timer.setInterval(handleBufferedData, 200);
-}
-
-std::string Client::serializeData(common::itf::Header& header, common::itf::Message& message)
-{
-    return headerSerializer.serialize(header) + "@" + messageSerializer.serialize(message) + "\n";
-}
-}
+} // namespace client
